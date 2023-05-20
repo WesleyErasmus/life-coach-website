17a9215f14a71bade8340b9d58346b223e7a49e7
Wesley Erasmus <wesley.erasmus119@gmail.com>
2023-05-20 07:36:08 +0200

    Update GetInTouch.vue
    Added a contact me form

--- src/components/GetInTouch.vue
+++ src/components/GetInTouch.vue
@@ -14,23 +14,88 @@
                         </div>
                     </v-card-item>
                     <v-card-actions class="contact-btn-container">
-                        <v-btn class="rounded-pill px-8 py-2" variant="elevated" size="large">
-                            <a href="mailto:daryl@ontrackcoaching.co.za?subject=Online Coaching Enquiry"
-                                class="text-decoration-none text-black">
-                                Get In Touch
-                            </a>
+                        <v-btn class="rounded-pill px-8 py-2" variant="elevated" size="large" @click="dialog = true">
+                            <!-- <a href="mailto:daryl@ontrackcoaching.co.za?subject=Online Coaching Enquiry"
+                                class="text-decoration-none text-black"> -->
+                            Get In Touch
+                            <!-- </a> -->
                         </v-btn>
                     </v-card-actions>
                 </div>
             </v-card>
         </v-container>
     </v-card>
+
+    <!-- Get in touch form -->
+
+    <v-row justify="center">
+        <v-dialog v-model="dialog" persistent width="1024">
+            <v-card>
+                <v-card-title class="pa-0">
+                    <!-- Dialog Title -->
+                    <v-toolbar dark color="primary">
+                        <v-toolbar-title>Get In Touch</v-toolbar-title>
+                        <v-spacer></v-spacer>
+                        <!-- Dialog close button -->
+                        <v-btn icon dark @click="dialog = false">
+                            <v-icon>mdi-close</v-icon>
+                        </v-btn>
+                    </v-toolbar>
+                </v-card-title>
+                <v-card-text>
+                    <v-container>
+                        <v-row>
+                            <v-col cols="12" sm="6" md="6">
+                                <!-- First Name Input -->
+                                <v-text-field label="First name*" required></v-text-field>
+                            </v-col>
+                            <v-col cols="12" sm="6" md="6">
+                                <!-- Last Name Input -->
+                                <v-text-field label="Last name*" required></v-text-field>
+                            </v-col>
+
+                            <!-- Email Input -->
+                            <v-col cols="12">
+                                <v-text-field label="Email*" required></v-text-field>
+                            </v-col>
+                            <v-col cols="12" sm="12">
+                                <!-- Select Services -->
+                                <v-autocomplete
+                                    :items="['Setting Your Course', 'Getting Your Career Back On Track', 'Finding Traction In Your Relationships', 'Thinking Like a Leader']"
+                                    label="Select Coaching Services*" multiple required
+                                    hint="You can select more than one"></v-autocomplete>
+                            </v-col>
+                            <!-- Message Textarea -->
+                            <v-col cols="12">
+                                <v-textarea label="Message*" required></v-textarea>
+                            </v-col>
+                        </v-row>
+                    </v-container>
+                    <!-- Required note -->
+                    <small>*indicates required field</small>
+                </v-card-text>
+                <v-card-actions>
+                    <v-spacer></v-spacer>
+                    <v-btn color="" variant="text" @click="dialog = false">
+                        Close
+                    </v-btn>
+                    <v-btn color="primary" variant="text" @click="dialog = false">
+                        Send
+                    </v-btn>
+                </v-card-actions>
+            </v-card>
+        </v-dialog>
+    </v-row>
+
+    <!-- End of get in touch form -->
 </template>
 
 <script>
 export default {
     data() {
-        return {};
+        return {
+            dialog: false,
+        };
     },
 };
 </script>
